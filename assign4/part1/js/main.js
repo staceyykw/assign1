5ce74c121d4ce02c4c16f9dcbae5aa1b68e836f8
staceyykw <wilscamdnd@gmail.com>
2025-12-12 22:34:27 -0500

    Update main.js

--- assign4/part1/js/main.js
+++ assign4/part1/js/main.js
@@ -1,4 +1,4 @@
-// Complete variable definitions and random function
+// Complete variable definitions and random functions
 
 const customName = document.getElementById("custom-name");
 const generateBtn = document.querySelector(".generate");
@@ -9,47 +9,43 @@ function randomValueFromArray(array) {
   return array[random];
 }
 
-// Solution: Raw text strings
+// Raw text strings
 
-const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const places = ["the soup kitchen", "Disneyland", "the White House"];
-const events = [
-  "spontaneously combusted",
-  "melted into a puddle on the sidewalk",
-  "turned into a slug and slithered away",
-];
+// Willy the Goblin
+// Big Daddy
+// Father Christmas
 
-// Solution: Partial return random string function
+// the soup kitchen
+// Disneyland
+// the White House
+
+// spontaneously combusted
+// melted into a puddle on the sidewalk
+// turned into a slug and slithered away
+
+// Partial return random string function
 
 function returnRandomStoryString() {
-  const randomCharacter = randomValueFromArray(characters);
-  const randomPlace = randomValueFromArray(places);
-  const randomEvent = randomValueFromArray(events);
-
-  let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
+  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
   return storyText;
 }
 
-// Solution: Event listener and partial generate function definition
+// Event listener and partial generate function definition
 
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
-  let newStory = returnRandomStoryString();
-
   if (customName.value !== "") {
     const name = customName.value;
-    newStory = newStory.replace("Bob", name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = `${Math.round(300 / 14)} stone`;
-    const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
-    newStory = newStory.replace("300 pounds", weight);
-    newStory = newStory.replace("94 Fahrenheit", temperature);
+    const weight = Math.round(300);
+    const temperature = Math.round(94);
   }
 
-  story.textContent = newStory;
+  // TODO: replace "" with the correct expression
+  story.textContent = "";
   story.style.visibility = "visible";
 }
