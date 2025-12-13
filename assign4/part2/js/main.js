172e540d015060f3a02bffe5bbb732be356ebf6e
staceyykw <wilscamdnd@gmail.com>
2025-12-13 18:32:51 -0500

    commit 2-2 (js)
    added a description/information at the top in the form of a javascript comment

--- assign4/part2/js/main.js
+++ assign4/part2/js/main.js
@@ -1,5 +1,14 @@
+/*
+Name: Stacey Wilson
+File: main.js
+Date: 13 December 2025
+Javascript file for creating an array, creating thumbnails with a loop, 
+and mapping, controlling and using a function for using our image gallery.
+*/
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
